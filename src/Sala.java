fc82e277cc676a459c82e3129e53b191f944f1ee
Leonardo Almeida <leonardoaesilva@outlook.com>
2023-01-20 19:01:10 -0300

    mudanças no atributo de alocação de sala

--- src/Sala.java
+++ src/Sala.java
@@ -1,27 +1,15 @@
 public abstract class Sala {
-    private boolean alocada;
     private String ala;
     private String bloco;
 
-    public Sala() {
-        this.alocada = false;
-    }
+    public Sala() {}
 
     public Sala(String ala, String bloco) {
-        this.alocada = false;
         this.ala = ala;
         this.bloco = bloco;
     }
 
     public abstract void reservar(int dd, int mm, int aa);
-
-    public boolean isAlocada() {
-        return alocada;
-    }
-
-    public void setAlocada(boolean alocada) {
-        this.alocada = alocada;
-    }
 
     public String getAla() {
         return ala;
--- src/SalaCirurgia.java
+++ src/SalaCirurgia.java
@@ -1,17 +1,27 @@
 public class SalaCirurgia extends Sala {
+    private boolean alocada;
+
     public SalaCirurgia(String ala, String bloco) {
         super(ala, bloco);
     }
 
     @Override
     public void reservar(int dd, int mm, int aa) {
-        if (super.isAlocada())
+        if (isAlocada())
             System.out.println("Sala já reservada!");
         else if ((dd <= 0 || dd > 31) || (mm <= 0 || mm > 12) || (aa < 0 || aa > 99))
             System.out.println("Data inválida.");
         else {
             System.out.println("Sala reservada com sucesso para esta data.");
-            super.setAlocada(true);
+            setAlocada(true);
         }
+    }
+
+    public boolean isAlocada() {
+        return alocada;
+    }
+
+    public void setAlocada(boolean alocada) {
+        this.alocada = alocada;
     }
 }
